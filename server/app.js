502ede7c58420fa40b65942ffc2a23a1a48ce8dc
kufaii <ahmd.fqrr@gmail.com>
2024-04-24 11:03:06 +0700

    feat:real-time post

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 if(process.env.NODE_ENV !== "production"){
     require('dotenv').config()
 }
+const {User, Post, Comment, Category, Follow} = require('./models')
 
 const cors = require('cors');
 const express = require('express');
@@ -10,17 +11,57 @@ const errorHandler = require('./middlewares/errorHandler');
 const app = express()
 const port = process.env.PORT || 3000
 
+const { createServer } = require('http')
+const { Server } =  require("socket.io")
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.use(cors())
 app.use(router)
-
-
 app.use(errorHandler)
 
-app.listen(port, () => {
-  console.log(`RUNNNNNNNNIIIINNNNGGGGGG on port ${port}`)
+const httpServer = createServer(app)
+const io = new Server(httpServer,{
+  cors:{
+    origin: "*"
+  }
 })
+let useridddd = 0
+io.on('connection', (socket)=>{
+  console.log("access token >>>>>>>>>>>>>>>>>>",socket.handshake.auth.access_token);
+
+  socket.on("new-post", async ()=>{
+    try {
+      const allPost = await Post.findAll({attributes: { exclude: ['UserId'] }, order:[['votes', 'DESC']]})
+      socket.broadcast.emit("post-new", allPost)
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  
+  // io.use(async (socket, next) => {
+  //   if(!socket.handshake.auth.access_token) throw { name: "unauthenticated" }
+
+  //       const token = socket.handshake.auth.access_token
+
+  //       const payload = verifyToken(token)
+
+  //       const findUser = await User.findByPk(payload.id)
+  //       if(!findUser) throw { name: "unauthenticated" }
+
+  //       useridddd = payload.id
+  //     });
+  //     next();
+})
+
+
+
+
+// app.listen(port, () => {
+//   console.log(`RUNNNNNNNNIIIINNNNGGGGGG on port ${port}`)
+// })
+
+httpServer.listen(port, () => console.log(`RUNNNNNNNNIIIINNNNGGGGGG on port ${port}`))
 
 module.exports = app
